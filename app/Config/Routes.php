556f0412bb3b9841bb12be1368bf8911f08f7153
Aulia Vira <viraaulia694@example.com>
2024-02-21 19:13:17 +0700

    Pesan commit Anda di sini

---
+++ app/Config/Routes.php
@@ -0,0 +1,16 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+//produk
+$routes->get('/produk', 'Produk::index');
+$routes->get('/produk/tampil', 'Produk::ambilSemua');
+$routes->post('/produk/simpan', 'Produk::simpan');
+$routes->get('/produk/edit', 'Produk::edit');
+$routes->post('/produk/update', 'Produk::update');
+$routes->post('/produk/delete', 'Produk::delete');
---
+++ app/Models/ProdukModel.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ProdukModel extends Model
+{
+    protected $table            = 'tb_produk';
+    protected $primaryKey       = 'id_produk';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'object';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ["id_produk", "nama_produk", "harga", "stok"];
+
+    protected bool $allowEmptyInserts = false;
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
